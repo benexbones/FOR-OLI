74b618c66245ddc56bd8bfd18692cbe4cb96f0e8
John Benedick Magalona <benedickmagalona26@gmail.com>
2025-02-13 23:56:55 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY VALENTINES!').split('')
+    const titles = ('HAPPY VALENTINES OLI!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
